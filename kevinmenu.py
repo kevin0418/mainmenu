24e367b889852beb9bdf5b1d33573369a061fbd2
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2025-03-03 21:43:37 +1000

    add 1st time upload
    add  1st time upload  kevinmenu

---
+++ kevinmenu.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.write("# Welcome to Kevin 의  챗봇 ! 👋")
+
+with st.sidebar:
+     
+     st.markdown(
+      """
+          아래 를 선택 하시고 음미 해보세요
+      
+          - [2023년 X-Mas Card ](https://youtu.be/-i3qjuYwkkI)
+          - [결혼 43주년 ](https://youtu.be/z_BM9oS_tVQ)
+          
+          - [2023 Youth REM ](https://youtu.be/2l8QLk4pE_4)
+          - [나도 부활할수 있나 ](https://youtu.be/HT-7nTMv8Ak)
+          - [용서 ](https://youtu.be/Pt5_qFeGl3Q)
+          - [상상력 의 힘 ](https://youtu.be/4bSIVsGl7GM)
+          - [우환 때문에 산다 ](https://www.youtube.com/watch?v=0FV7rqP06c8)
+                 
+    """
+    )
+     
+
+st.markdown(
+    """
+    살다 보면 즐거운 일도 있지만 말못할 고민이 있게 됩니다.
+    사람한태 물어 보가 힘들때   
+    여기 Kevin 의 심리 상담 챗봇 한테 문의를 ..
+
+
+    - [심리 상담 챗봇 ](https://2024-app4-8yc8qc8xa6jacxc3mjtyw4.streamlit.app/)
+    
+    
+    업무를 하다보면 문서를 번역 한다던지 요약한다던지 내용의 의미를 묻고 싶을때 
+    Kevin 의 챗봇을 통해 물어 보세요.  먼저 볼 File 를 upload 하시고   
+    
+    - [Kevin 의 챗봇 ](https://langchain6-gwtklymlqjbtytbcaestpf.streamlit.app/)
+
+    
+"""
+)
